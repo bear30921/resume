65e1719f83ca527189278ecce2f27d8f7a1fbb10
bear30921 <bear30921@gmail.com>
2017-09-27 18:06:32 +0800

    add responsive javascript

--- javascript/main.js
+++ javascript/main.js
@@ -1,13 +1,17 @@
 $(function () {
 
-  $(window).on('scroll', function () {
-    var mq = window.matchMedia("(min-width: 768px)");
-    var scroll = $(window).scrollTop();
+  var s = skrollr.init();
 
-    //供768px尺寸以上使用
-    if (mq.matches) {
+  var mq = window.matchMedia("(min-width: 768px)");
 
-      // 
+  // 供768px尺寸以上使用
+  if (mq.matches) {
+
+
+    $(window).on('scroll', function () {
+      var scroll = $(window).scrollTop();
+      // console.log(scroll);
+      // 固定導覽列
       if (scroll >= 478) {
         $('.nav').addClass('active');
         $('#header').addClass('active');
@@ -15,19 +19,30 @@ $(function () {
         $('.nav').removeClass('active');
         $('#header').removeClass('active');
       }
+    });
 
-      //供767px尺寸以下使用
-    } else {
 
+
+    // 供767px尺寸以下使用
+  } else {
+
+    // 導覽列增加效果
+    $(window).on('scroll', function () {
+      var scroll = $(window).scrollTop();
       if (scroll > 0) {
         $('.side-bar').addClass('active');
       } else {
         $('.side-bar').removeClass('active');
       }
+    });
 
-    }
+  }
+  // 不分尺寸使用
 
-    // 不分尺寸使用
+
+  // top按鈕，特效
+  $(window).on('scroll', function () {
+    var scroll = $(window).scrollTop();
     if (scroll >= 478) {
       $('#top').css({
         'opacity': 1,
@@ -42,11 +57,10 @@ $(function () {
 
   });
 
-
-  // 調整瀏覽器視窗，移除殘餘的class
+  // 調整瀏覽器視窗時，移除殘餘的class
   $(window).on('resize', function () {
-    var viewpoint = $(window).width();
-    if (viewpoint >= 768) {
+    var screenWidth = $(window).width();
+    if (screenWidth >= 768) {
       $('.side-bar').removeClass('active');
     } else {
       $('.nav').removeClass('active');
@@ -62,8 +76,6 @@ $(function () {
       scrollTop: $(currentPosition).offset().top - 63
     }, 1000);
   });
-
-
 
 
 
