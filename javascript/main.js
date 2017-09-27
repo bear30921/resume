1fc2eabb34714c56b088b48f1a39c493ac9a12db
bear30921 <bear30921@gmail.com>
2017-09-27 12:27:41 +0800

    add resize event to js

--- javascript/main.js
+++ javascript/main.js
@@ -1,41 +1,60 @@
 $(function () {
 
   $(window).on('scroll', function () {
-    var mq = window.matchMedia("(min-width: 991px)");
-    var viewpoint = $(window).scrollTop();
-    
-    // 大螢幕套用此javascript
+    var mq = window.matchMedia("(min-width: 768px)");
+    var scroll = $(window).scrollTop();
+
+    //供768px尺寸以上使用
     if (mq.matches) {
 
-      if (viewpoint >= 478) {
+      // 
+      if (scroll >= 478) {
         $('.nav').addClass('active');
         $('#header').addClass('active');
-        $('#top').css({
-          'opacity': 1,
-          'right': '12px'
-        });
       } else {
         $('.nav').removeClass('active');
         $('#header').removeClass('active');
-        $('#top').css({
-          'opacity': 0,
-          'right': '-100px'
-        });
       }
 
-      //平板、手機套用此javascript
+      //供767px尺寸以下使用
     } else {
-      if (viewpoint > 0) {
+
+      if (scroll > 0) {
         $('.side-bar').addClass('active');
       } else {
         $('.side-bar').removeClass('active');
       }
+
+    }
+
+    // 不分尺寸使用
+    if (scroll >= 478) {
+      $('#top').css({
+        'opacity': 1,
+        'right': '12px'
+      });
+    } else {
+      $('#top').css({
+        'opacity': 0,
+        'right': '-100px'
+      });
     }
 
   });
 
 
+  // 調整瀏覽器視窗，移除殘餘的class
+  $(window).on('resize', function () {
+    var viewpoint = $(window).width();
+    if (viewpoint >= 768) {
+      $('.side-bar').removeClass('active');
+    } else {
+      $('.nav').removeClass('active');
+      $('#header').removeClass('active');
+    }
+  });
 
+  // 點擊按鈕，做順暢的滑動
   $('.move').on('click', function (e) {
     e.preventDefault();
     var currentPosition = $(this).attr('href');
@@ -43,20 +62,6 @@ $(function () {
       scrollTop: $(currentPosition).offset().top - 63
     }, 1000);
   });
-
-
-  $('#top').on('click', function (e) {
-    e.preventDefault();
-    $('html, body').stop().animate({
-      scrollTop: 0
-    }, 1000);
-  });
-
-
-
-
-
-
 
 
 
