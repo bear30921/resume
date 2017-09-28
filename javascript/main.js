afaef8a8445958659450d0717dadc81598c4018d
bear30921 <bear30921@gmail.com>
2017-09-28 16:32:49 +0800

    add rwd button switch effect

--- javascript/main.js
+++ javascript/main.js
@@ -1,17 +1,16 @@
 $(function () {
 
-  var s = skrollr.init();
+
 
   var mq = window.matchMedia("(min-width: 768px)");
 
   // 供768px尺寸以上使用
   if (mq.matches) {
-
+    var s = skrollr.init();
 
     $(window).on('scroll', function () {
       var scroll = $(window).scrollTop();
-      // console.log(scroll);
-      // 固定導覽列
+
       if (scroll >= 478) {
         $('.nav').addClass('active');
         $('#header').addClass('active');
@@ -21,12 +20,10 @@ $(function () {
       }
     });
 
-
-
     // 供767px尺寸以下使用
   } else {
 
-    // 導覽列增加效果
+
     $(window).on('scroll', function () {
       var scroll = $(window).scrollTop();
       if (scroll > 0) {
@@ -36,39 +33,43 @@ $(function () {
       }
     });
 
+
+    $('.menu').on('click', function () {
+      $(this).toggleClass('active');
+      $('.menu-list').toggleClass('active');
+
+    });
+
+
   }
+
+
   // 不分尺寸使用
 
-
-  // top按鈕，特效
   $(window).on('scroll', function () {
     var scroll = $(window).scrollTop();
     if (scroll >= 478) {
-      $('#top').css({
-        'opacity': 1,
-        'right': '12px'
-      });
+      $('#top').addClass('active');
     } else {
-      $('#top').css({
-        'opacity': 0,
-        'right': '-100px'
-      });
+      $('#top').removeClass('active');
     }
-
   });
 
-  // 調整瀏覽器視窗時，移除殘餘的class
+
   $(window).on('resize', function () {
     var screenWidth = $(window).width();
     if (screenWidth >= 768) {
+      var s = skrollr.init();
       $('.side-bar').removeClass('active');
     } else {
+      var s = skrollr.init();
+      s.destroy();
       $('.nav').removeClass('active');
       $('#header').removeClass('active');
     }
   });
 
-  // 點擊按鈕，做順暢的滑動
+
   $('.move').on('click', function (e) {
     e.preventDefault();
     var currentPosition = $(this).attr('href');
@@ -76,6 +77,9 @@ $(function () {
       scrollTop: $(currentPosition).offset().top - 63
     }, 1000);
   });
+
+
+
 
 
 
