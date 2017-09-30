943635f769adc04e5028431d4abd7352b0247d74
bear30921 <bear30921@gmail.com>
2017-09-30 16:50:16 +0800

    fix some bug

--- javascript/main.js
+++ javascript/main.js
@@ -1,84 +1,76 @@
 $(function () {
 
 
-
-  var mq = window.matchMedia("(min-width: 768px)");
-
-  // 供768px尺寸以上使用
-  if (mq.matches) {
-    var s = skrollr.init();
-
-    $(window).on('scroll', function () {
-      var scroll = $(window).scrollTop();
-
-      if (scroll >= 478) {
-        $('.nav').addClass('active');
-        $('#header').addClass('active');
-      } else {
-        $('.nav').removeClass('active');
-        $('#header').removeClass('active');
-      }
-    });
-
-    // 供767px尺寸以下使用
-  } else {
-
-
-    $(window).on('scroll', function () {
-      var scroll = $(window).scrollTop();
-      if (scroll > 0) {
-        $('.side-bar').addClass('active');
-      } else {
-        $('.side-bar').removeClass('active');
-      }
-    });
-
-
-    $('.menu').on('click', function () {
-      $(this).toggleClass('active');
-      $('.menu-list').toggleClass('active');
-
-    });
-
-
-  }
-
-
-  // 不分尺寸使用
-
-  $(window).on('scroll', function () {
-    var scroll = $(window).scrollTop();
-    if (scroll >= 478) {
-      $('#top').addClass('active');
-    } else {
-      $('#top').removeClass('active');
-    }
-  });
-
-
-  $(window).on('resize', function () {
-    var screenWidth = $(window).width();
-    if (screenWidth >= 768) {
+  function enableSkrollr(str) {
+    if (str == 'active') {
       var s = skrollr.init();
-      $('.side-bar').removeClass('active');
     } else {
       var s = skrollr.init();
       s.destroy();
+    }
+  }
+
+  enableSkrollr('active');
+
+  // 固定選單
+  $(window).on('scroll', function () {
+    var $scroll = $(window).scrollTop();
+    if ($scroll >= 478) {
+      $('.nav').addClass('active');
+      $('#header').addClass('active');
+    } else {
       $('.nav').removeClass('active');
       $('#header').removeClass('active');
     }
   });
 
 
-  $('.move').on('click', function (e) {
-    e.preventDefault();
-    var currentPosition = $(this).attr('href');
-    $('html, body').stop().animate({
-      scrollTop: $(currentPosition).offset().top - 63
-    }, 1000);
+  $(window).on('scroll', function () {
+    var $scroll = $(window).scrollTop();
+    if ($scroll > 0) {
+      $('.side-bar').addClass('active');
+    } else {
+      $('.side-bar').removeClass('active');
+    }
   });
 
 
+  // 不分尺寸，都可以使用 --------------------------
+
+
+  $(window).on('resize', function () {
+    if ($(window).width() <= 767) {
+      enableSkrollr();
+    } else {
+      enableSkrollr('active');
+    }
+  });
+  //  top按鈕動畫
+  $(window).on('scroll', function () {
+    var $scroll = $(window).scrollTop();
+    if ($scroll >= 478) {
+      $('#top').addClass('active');
+    } else {
+      $('#top').removeClass('active');
+    }
+  });
+
+  // 選單移動指定位置動畫
+  $('.move').on('click', function (e) {
+    e.preventDefault();
+    var $currentPosition = $(this).attr('href');
+    $('html, body').stop().animate({
+      scrollTop: $($currentPosition).offset().top - 63
+    }, 1000);
+    $('.menu').removeClass('active');
+    $('.menu-list').removeClass('active');
+  });
+
+  // 手機板選單切換
+  $('.menu').on('click', function () {
+    $(this).toggleClass('active');
+    $('.menu-list').toggleClass('active');
+  });
 
 
 
